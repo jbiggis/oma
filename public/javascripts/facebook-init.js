174c268558093c466a1c3460ee133a3e06f91772
Jonathan <jonathan@ubuntu.(none)>
2011-03-13 05:26:03 -0400

    added facebook init js

---
+++ public/javascripts/facebook-init.js
@@ -0,0 +1,7 @@
+
+  FB.init({
+	appId  : '202763786417566',
+	status : true, // check login status
+	cookie : true, // enable cookies to allow the server to access the session
+	xfbml  : true  // parse XFBML
+  });
