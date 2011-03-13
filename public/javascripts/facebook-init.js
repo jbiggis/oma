e1ffecb1458541e0dc40bd519d53014e74ef94a7
Jonathan <jonathan@ubuntu.(none)>
2011-03-13 05:27:51 -0400

    changed fb init facebook app id

--- public/javascripts/facebook-init.js
+++ public/javascripts/facebook-init.js
@@ -1,6 +1,6 @@
 
   FB.init({
-	appId  : '202763786417566',
+	appId  : '196091917091198',
 	status : true, // check login status
 	cookie : true, // enable cookies to allow the server to access the session
 	xfbml  : true  // parse XFBML
